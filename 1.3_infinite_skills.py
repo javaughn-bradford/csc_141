7f1744be4982a1f3e93fbee6d2e7cf7c3e5bef61
javaughn-bradford <javaughn.bradford001@albright.edu>
2024-09-01 19:35:17 -0700

    Add files via upload

---
+++ 1.3_infinite_skills.py
@@ -0,0 +1,8 @@
+1.3.infinite_skills
+'''
+Unfortunately, I do not have any infinite programming skills. 
+I barely have any regular programming skills at all. But taking this class will help me achieve this goal.
+My end goal is to become a good if not great programmer and learn as many codes as possible. 
+I want to make a new website for myself thats about me. I also wanna code and make an app.
+It could be for a game or just informational
+''' 
