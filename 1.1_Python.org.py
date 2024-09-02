2da8a88b94a4527d2df47f2c1581835fdba83c26
javaughn-bradford <javaughn.bradford001@albright.edu>
2024-09-01 17:49:41 -0700

    Add files via upload

---
+++ 1.1_Python.org.py
@@ -0,0 +1,9 @@
+1.1_Python.org 
+''' 
+Python has a lot of different thing to offer. 
+They offer jobs amd how to get started in the python world. 
+What I like the most about pyton is that your able to join Community network and learn from professional
+I am new to python and everything coding so I excited for really everything 
+One thing that would be useful for me is the general question helpdesk
+It is used for asking questions about python and its general information to learna nd get better at coding.
+'''
