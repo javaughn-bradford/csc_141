d93c1c13358b7258403b05d6a481e6d8c3e32c93
javaughn-bradford <javaughn.bradford001@albright.edu>
2024-09-05 08:50:26 -0400

    Add files via upload

---
+++ 02_variables_and_data_types/02_10_favorite_number.py
@@ -0,0 +1,3 @@
+#02_10_favorite_number 
+favorite_number= "7"
+print("My favorite number is " + (favorite_number))
---
+++ 02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,8 @@
+#02_11_adding_comments 
+#Javaughn Bradford 
+# One program I did was printing names and quotes into one sentence. 
+# The fun thing was that I had to print them from 2 different variables from 1 line 
+
+#Javaughn Bradford : 
+#Another program that was nice to do was the adding. 
+#Its crazy on how the code and print whatever you say and also be used as a calculator calculate things you put. 
