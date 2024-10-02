4671687134240c26ab79da2b36200fef61a313c6
javaughn-bradford <javaughn.bradford001@albright.edu>
2024-10-02 10:00:21 -0400

    Create 05_03_test.py

---
+++ 05_if_statements/05_03_test.py
@@ -0,0 +1,14 @@
+# 05_03_alien_colors
+color = "green"
+print("What color is the Alien, *green*")
+if color == "green":
+    print("You won! + 5 points!")
+elif color == "yellow":
+    print("No")
+    
+color = "yellow"
+print("What color is the Alien, *yellow*")
+if color == "green":
+    print("You won! + 5 points!")
+elif color == "yellow":
+    print("No")
